c9027e23fa3a1eb7bb9c2126b5930b6793587d89
Xuan Bich MAI THI <xuanbichmai@gmail.com>
2017-02-09 12:29:31 +0100

    Validation association done

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail_id, :uniqueness => { :scope => :ingredient_id }
+end
